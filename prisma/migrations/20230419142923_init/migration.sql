6a61d6976571962fbc635d4a7257273135b78b7a
Jake <jakefaalam74@gmail.com>
2023-04-19 22:44:49 +0800

    message

--- prisma/migrations/20230419142923_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `report` ADD COLUMN `link` VARCHAR(191) NOT NULL DEFAULT '';
---
+++ prisma/migrations/20230419144321_init/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "report" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "latitude" DOUBLE PRECISION NOT NULL,
+    "longitude" DOUBLE PRECISION NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" TEXT NOT NULL DEFAULT 'Unrescued',
+    "link" TEXT NOT NULL DEFAULT '',
+
+    CONSTRAINT "report_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "user"("email");
